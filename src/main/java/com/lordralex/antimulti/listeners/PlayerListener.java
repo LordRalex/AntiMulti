b9960418b48fd6aebc973253a7e5ebae4c098481
LordRalex <lordralex@gmail.com>
2012-03-20 03:48:24 -0500

    Fixed quit system

--- src/main/java/com/lordralex/antimulti/listeners/PlayerListener.java
+++ src/main/java/com/lordralex/antimulti/listeners/PlayerListener.java
@@ -5,16 +5,49 @@
 package com.lordralex.antimulti.listeners;
 
 import com.lordralex.antimulti.AntiMulti;
+import com.lordralex.antimulti.data.AMPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  *
  * @author Joshua
  */
 public class PlayerListener implements Listener{
+    
+    AntiMulti plugin;
 
     public PlayerListener(AntiMulti aThis) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        plugin = aThis;
     }
     
+    @EventHandler (priority=EventPriority.NORMAL)
+    public void onPlayerQuit(PlayerQuitEvent event)
+    {
+        Player player = event.getPlayer();
+        if(player != null) {
+            for(AMPlayer amplayer: plugin.playersOnServer)
+            {
+                if(amplayer != null)
+                {
+                    if(player.equals(amplayer.player))
+                    {
+                        if(!amplayer.loggedIn)
+                            event.setQuitMessage(null);
+                        amplayer = null;
+                    }
+                }
+            }
+        }
+    }
+    
+    @EventHandler (priority=EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        event.setJoinMessage(null);
+    }
 }
