a0c9fc92aec461d55fe4ca39079956c930b0208f
Joshua <lordralex@gmail.com>
2013-01-23 05:47:36 -0600

    Remove plugin dep.

--- src/main/java/com/lordralex/antimulti/utils/IPHandler.java
+++ src/main/java/com/lordralex/antimulti/utils/IPHandler.java
@@ -1,6 +1,5 @@
 package com.lordralex.antimulti.utils;
 
-import com.lordralex.antimulti.logger.AMLogger;
 import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.List;
@@ -23,57 +22,52 @@ public final class IPHandler {
      * @return True if the ips are the same, false otherwise
      */
     public static boolean contains(String ip1, String ip2) {
-        try {
-            if (ip1.equalsIgnoreCase(ip2)) {
-                return true;
-            }
-            String[] ip1parts = ip1.split(".");
-            if (ip1parts.length != 4) {
-                String[] temp = new String[4];
-                Arrays.fill(temp, "*");
-                System.arraycopy(ip1parts, 0, temp, 0, ip1parts.length);
-                ip1parts = temp;
-            }
-            String[] ip2parts = ip1.split(".");
-            if (ip2parts.length != 4) {
-                String[] temp = new String[4];
-                Arrays.fill(temp, "*");
-                System.arraycopy(ip2parts, 0, temp, 0, ip2parts.length);
-                ip2parts = temp;
-            }
-            for (int i = 0; i < 4; i++) {
-                String partA = ip1parts[i];
-                String partB = ip2parts[i];
-                if (partA.contains("-")) {
-                    try {
-                        int num1 = Integer.parseInt(partA.split("-")[0]);
-                        int num2 = Integer.parseInt(partA.split("-")[1]);
-                        int test = Integer.parseInt(partB);
-                        if (num1 <= test && test <= num2) {
-                        }
-                    } catch (NumberFormatException e) {
-                        return false;
-                    } catch (IndexOutOfBoundsException e) {
-                        return false;
-                    }
-                }
-                if (!partA.equalsIgnoreCase("*") && !partB.equalsIgnoreCase("*")) {
-                    try {
-                        int one = Integer.parseInt(partA);
-                        int two = Integer.parseInt(partB);
-                        if (one != two) {
-                            return false;
-                        }
-                    } catch (NumberFormatException e) {
-                        return false;
-                    }
-                }
-            }
+        if (ip1.equalsIgnoreCase(ip2)) {
             return true;
-        } catch (Exception e) {
-            AMLogger.error(e);
-            return false;
         }
+        String[] ip1parts = ip1.split(".");
+        if (ip1parts.length != 4) {
+            String[] temp = new String[4];
+            Arrays.fill(temp, "*");
+            System.arraycopy(ip1parts, 0, temp, 0, ip1parts.length);
+            ip1parts = temp;
+        }
+        String[] ip2parts = ip1.split(".");
+        if (ip2parts.length != 4) {
+            String[] temp = new String[4];
+            Arrays.fill(temp, "*");
+            System.arraycopy(ip2parts, 0, temp, 0, ip2parts.length);
+            ip2parts = temp;
+        }
+        for (int i = 0; i < 4; i++) {
+            String partA = ip1parts[i];
+            String partB = ip2parts[i];
+            if (partA.contains("-")) {
+                try {
+                    int num1 = Integer.parseInt(partA.split("-")[0]);
+                    int num2 = Integer.parseInt(partA.split("-")[1]);
+                    int test = Integer.parseInt(partB);
+                    if (num1 <= test && test <= num2) {
+                    }
+                } catch (NumberFormatException e) {
+                    return false;
+                } catch (IndexOutOfBoundsException e) {
+                    return false;
+                }
+            }
+            if (!partA.equalsIgnoreCase("*") && !partB.equalsIgnoreCase("*")) {
+                try {
+                    int one = Integer.parseInt(partA);
+                    int two = Integer.parseInt(partB);
+                    if (one != two) {
+                        return false;
+                    }
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /**
@@ -116,8 +110,8 @@ public final class IPHandler {
     }
 
     /**
-     * Checks a list of IPs to see if one is in it. This also checks for the wildcard (*)
-     * and ranges (x-y)
+     * Checks a list of IPs to see if one is in it. This also checks for the
+     * wildcard (*) and ranges (x-y)
      *
      * @param list The list of IPs to check with
      * @param ip The ip to attempt to find
