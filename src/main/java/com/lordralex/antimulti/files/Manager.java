a2a8b1b895a14c35196eda4acce854424aeac48f
LordRalex <lordralex@gmail.com>
2013-07-06 02:57:01 -0500

    Add license

--- src/main/java/com/lordralex/antimulti/files/Manager.java
+++ src/main/java/com/lordralex/antimulti/files/Manager.java
@@ -1,16 +1,32 @@
+/*
+ * Copyright (C) 2013 Lord_Ralex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lordralex.antimulti.files;
 
 public interface Manager {
 
-    public abstract String[] getIPs(String name);
+    public String[] getIPs(String name);
 
-    public abstract String[] getNames(String ip);
+    public String[] getNames(String ip);
 
-    public abstract void addIP(String name, String ip);
+    public void addIP(String name, String ip);
 
-    public abstract void addName(String ip, String name);
+    public void addName(String ip, String name);
 
-    public abstract Manager setup();
+    public Manager setup();
 
-    public abstract void close();
+    public void close();
 }
